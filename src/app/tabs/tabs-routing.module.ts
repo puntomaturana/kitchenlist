0c0af2a5ecf888e586271aec2ef604178fdcce48
Washington Maturana <puntomaturana@gmail.com>
2021-06-02 13:56:26 -0400

    chg: avance de la clase 02 junio

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
         path: 'tab3',
         loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
       },
+      /*{
+        path: 'tab4',
+        loadChildren: () => import('../registro/registro.module').then(m => m.RegistroPageModule)
+      },*/
       {
         path: '',
         redirectTo: '/tabs/tab1',
